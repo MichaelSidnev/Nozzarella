0d65083e4617e2a6f5f8dc5ad6e93e27cb2b5e9c
user <cobalttft@yandex.ru>
2023-07-30 17:54:24 +0300

    working without html

--- src/main/java/com/nozzarella/configuration/SecurityConfiguration.java
+++ src/main/java/com/nozzarella/configuration/SecurityConfiguration.java
@@ -54,6 +54,7 @@ public class SecurityConfiguration {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/auth/**").permitAll();
+                auth.requestMatchers("/h2-console/**").permitAll();
                 auth.requestMatchers("/admin/**").hasRole("ADMIN");
                 auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
                 auth.anyRequest().authenticated();
